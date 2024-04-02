c9b8802f0b2547eb0a9f2c26f99ef828dae13733
Frankie <26727138+frankievalentine@users.noreply.github.com>
2024-04-02 12:08:39 -0700

    Initialize site

---
+++ astro.config.ts
@@ -0,0 +1,68 @@
+import { defineConfig } from "astro/config"
+import fs from "fs"
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import prefetch from "@astrojs/prefetch"
+
+import db from "@astrojs/db"
+import react from "@astrojs/react"
+import markdoc from "@astrojs/markdoc"
+import keystatic from "@keystatic/astro"
+
+import icon from "astro-icon"
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://v3frankie.me/",
+  markdown: {
+    remarkPlugins: [],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""],
+      },
+    },
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      wrap: true,
+    },
+  },
+  integrations: [
+    mdx({}),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    prefetch(),
+    db(),
+    react(),
+    markdoc(),
+    keystatic(),
+    icon(),
+  ],
+  output: "hybrid",
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+})
+function rawFonts(ext: Array<string>) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id)
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        }
+      }
+    },
+  }
+}
---
+++ keystatic.config.ts
@@ -0,0 +1,21 @@
+// keystatic.config.ts
+import { config, fields, collection } from "@keystatic/core"
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+  collections: {
+    posts: collection({
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        publishedDate: fields.date({ label: "Published Date" }),
+        content: fields.mdx({ label: "Content" }),
+      },
+    }),
+  },
+})
