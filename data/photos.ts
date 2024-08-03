b85e644ab91fd9ae18c71c54233a9b8cd17edc2d
Frankie <26727138+frankievalentine@users.noreply.github.com>
2024-08-02 19:52:06 -0700

    Add photos and newsletter components and functionality

---
+++ data/photos.ts
@@ -0,0 +1,67 @@
+interface PhotoProps {
+  image: string;
+  alt: string;
+}
+
+export const photos: PhotoProps[] = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1678757603916-a63283ffe93d?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "blue car parked in a parking lot",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1678762389792-9f85f6509ea9?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "womand holding a baby in her arms",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1678757604007-da979fc8aaaa?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "desk setup with monitor and laptop",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1678757604327-8b445513dc0c?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "sidewalk view with a row of benches and a coffee shop",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1512429234305-12fe5b0b0f07?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "macbook beside headphones and a plant",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1525477800099-be89f80b4dac?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "white audio technica wireless turntable near vinyl record sleeve",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1519001896312-85f41baa90a0?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "orange and white class a motorhome surrounded by snow",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1546550613-799015025b71?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "photography of the pacific ocean from mexico",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1527814050087-3793815479db?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "white and grey logitech g series cordless mouse on white surface",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1546550519-c8f2744e7438?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "blue and red stage lights",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1519236282064-01649736e20b?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "gray and white wooden house near green leaf tree",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1632179046409-e201ec20f079?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "a coffee shop with a neon sign above the counter",
+  },
+];
