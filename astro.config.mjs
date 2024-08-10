f5989c161f6dd5c8f6a2941f1f49ee20921bba00
Frankie <26727138+frankievalentine@users.noreply.github.com>
2024-08-09 17:10:37 -0700

    Removed Vite rollup options causing production build issue, and removed astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,38 +7,41 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import keystatic from "@keystatic/astro";
 import expressiveCode from "astro-expressive-code";
-import icon from "astro-icon";
+// import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://v3frankie.me",
   markdown: {},
   image: {
-    domains: ["images.unsplash.com"]
+    domains: ["images.unsplash.com"],
   },
   prefetch: true,
-  integrations: [sitemap(), robotsTxt(), partytown(), expressiveCode(), mdx(), markdoc(), keystatic(), icon(), react(), simpleStackForm(), tailwind()],
+  integrations: [
+    sitemap(),
+    robotsTxt(),
+    partytown(),
+    expressiveCode(),
+    mdx(),
+    markdoc(),
+    keystatic(),
+    react(),
+    simpleStackForm(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "hybrid",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
   // Cloudflare D1 platform proxy and use Astro's Image service with passthrough mode
   // adapter: cloudflare({
   //   platformProxy: true,
   //   imageService: "passthrough"
   // }),
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: "entry.[hash].mjs",
-          chunkFileNames: "chunks/chunk.[hash].mjs",
-          assetFileNames: "assets/asset.[hash][extname]"
-        }
-      }
-    },
-    plugins: []
-  }
+  plugins: [],
 });
